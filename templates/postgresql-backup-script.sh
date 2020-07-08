92295ba7ab0a1d6210fd2a122fef7ee88cab7265
L3D <l3d@c3woc.de>
2020-07-08 17:56:15 +0200

    Optionally add password

--- templates/postgresql-backup-script.sh
+++ templates/postgresql-backup-script.sh
@@ -15,6 +15,9 @@ create_backup_dir() {
 
 backup_databases() {
   {% for db in postgresql_backup.databases %}
+  {% if db.password -%}
+    export PGPASSWORD="{{ db.password }}"
+  {% endif %}
   if (umask 077 && pg_dump -F c -h "{{ db.host | default( 'localhost' ) }}" -U "{{ db.user | default( 'postgres' ) }}" -p "{{ db.port | default( '5432' ) }}" "{{ db.name }}" -f "{{ db.name }}.in_progress.psql"); then
 	  mv "{{ db.name }}.in_progress.psql" "{{ db.name }}.psql"
   else
