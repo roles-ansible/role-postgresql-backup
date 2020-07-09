00e4de0225cf5b08e0e707933fdce21c0e36da62
L3D <l3d@c3woc.de>
2020-07-09 10:45:12 +0200

    Change path and add compression

--- templates/postgresql-backup-script.sh
+++ templates/postgresql-backup-script.sh
@@ -19,8 +19,14 @@ backup_databases() {
   export PGPASSWORD="{{ db.password }}"
   {% endif %}
   if (umask 077 && pg_dump -F c -h "{{ db.host | default( 'localhost' ) }}" -U "{{ db.user | default( 'postgres' ) }}" -p "{{ db.port | default( '5432' ) }}" "{{ db.name }}" -f "{{ db.name }}.in_progress.psql"); then
+  {% if postgresql_backup.compress %}
+    tar -czvf  "{{ db.name }}.psql.tar.gz" "{{ db.name }}.in_progress.psql"
+  {% else %}
 	  mv "{{ db.name }}.in_progress.psql" "{{ db.name }}.psql"
+  {% endif %}
+    echo "backup of {{ db.name }} successful"
   else
+    echo "failed to export {{ db.name }}"
 	  return 1
   fi;
   {% endfor %}
